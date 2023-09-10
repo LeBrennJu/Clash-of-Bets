8d8ae28cd92d3a07185a0af57b8009d17144ad77
Julien Le Brenn <lebrennj@gmail.com>
2023-09-10 15:49:07 +0200

    add tableau pages

--- Clash-of-bets/src/pages/Array/Array.tsx
+++ Clash-of-bets/src/pages/Array/Array.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Versus from "./Versus";
-
+import "./Array.scss"
 const Array = () => {
 
 
     return (
         <>
+       
             <main className="array__container">
+
+            <div className="array-title_container"><h2>Parier</h2></div>
+            <div className="array-score_container">Points : <p> 600 </p></div>
+
+            <section>
                 <Versus />
+            </section>
+               
             </main>
         </>
     );
